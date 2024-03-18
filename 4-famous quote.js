114dacd99dc8761fea3f42a29afbdc61343c196e
Uzma-Abid <161968674+Uzma-Abid@users.noreply.github.com>
2024-03-19 01:32:57 +0500

    Add files via upload

---
+++ 4-famous quote.js
@@ -0,0 +1,5 @@
+// Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var auther = "Muhammad Ali Jinnah";
+var quote = "No struggle can ever succeed without women participating side by side with men.";
+console.log("".concat(auther, " once said,\"").concat(quote, "\""));
---
+++ 4-famous quote.ts
@@ -0,0 +1,7 @@
+// Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let auther : string = "Muhammad Ali Jinnah";
+let quote :string = "No struggle can ever succeed without women participating side by side with men.";
+
+console.log(`${auther} once said,"${quote}"`);
