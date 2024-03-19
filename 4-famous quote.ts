c6954ebeb9d612c0369088735fa4483e92c6e3af
Uzma-Abid <161968674+Uzma-Abid@users.noreply.github.com>
2024-03-19 06:29:47 +0500

    Update 4-famous quote.ts

--- 4-famous quote.ts
+++ 4-famous quote.ts
@@ -1,7 +1,7 @@
 // Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
-let auther : string = "Muhammad Ali Jinnah";
-let quote :string = "No struggle can ever succeed without women participating side by side with men.";
+let auther : string = "William Shakespeare";
+let quote :string = "My Words Fly Up, My Thoughts Remain Below. Words Without Thoughts Never To Heaven Go.";
 
 console.log(`${auther} once said,"${quote}"`);
