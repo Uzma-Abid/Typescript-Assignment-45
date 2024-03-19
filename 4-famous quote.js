75f128015546dd4cab6815e067472be77e806b43
Uzma-Abid <161968674+Uzma-Abid@users.noreply.github.com>
2024-03-19 06:28:27 +0500

    Update 4-famous quote.js

--- 4-famous quote.js
+++ 4-famous quote.js
@@ -1,5 +1,5 @@
 // Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-var auther = "Muhammad Ali Jinnah";
-var quote = "No struggle can ever succeed without women participating side by side with men.";
+var auther = "William Shakespeare ";
+var quote = "My Words Fly Up, My Thoughts Remain Below. Words Without Thoughts Never To Heaven Go..";
 console.log("".concat(auther, " once said,\"").concat(quote, "\""));
